f2f1918140fa2da1260175fb3f9ec35e3cbc85ce
Thientvse <thientvse@gmail.com>
2023-03-10 14:34:35 +0700

    add custom data

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+const queryString = require('query-string')
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
@@ -9,6 +10,7 @@ server.use(middlewares)
 // Add custom routes before JSON Server router
 server.get('/echo', (req, res) => {
   res.jsonp(req.query)
+
 })
 
 // To handle POST, PUT and PATCH you need to use a body-parser
@@ -18,11 +20,42 @@ server.use((req, res, next) => {
   if (req.method === 'POST') {
     req.body.createdAt = Date.now();
     req.body.updateAt = Date.now();
+  } else if (req.method === "PATCH"){
+    req.body.updateAt = Date.now();
   }
   // Continue to JSON Server router
   next()
 
-})
+});
+
+router.render = (req, res) => {
+  console.log.apply(req.query);
+
+  const header = res.getHeaders();
+  const totalCountHeader = header['x-total-count']
+
+  if(req.method === 'GET' && totalCountHeader){
+
+    const queryParams = queryString.parse(req._parsedUrl.query);
+
+    console.log(queryParams);
+
+
+    const result = {
+      data: res.locals.data,
+      pagination: {
+        _page: Number.parseInt(queryParams._page) || 1,
+        _limit: Number.parseInt(queryParams._limit) || 10,
+        _totalRows: Number.parseInt(totalCountHeader)
+      }
+    };
+
+    return res.jsonp(result);
+  }
+
+
+  res.jsonp(res.locals.data);
+};
 
 // Use default router
 server.use('/api',router)
